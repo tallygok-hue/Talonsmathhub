2152d06a2335de8bf846be1df1546aedbdd9193f
tallygok-hue <tallygok@gmail.com>
2026-02-05 19:48:11 -0700

    Create uv.config.js

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/uv/service/',
+    bare: 'https://bare.benrogu.org/',
+    encodeUrl: JavaScript.encodeUrl,
+    decodeUrl: JavaScript.decodeUrl,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
